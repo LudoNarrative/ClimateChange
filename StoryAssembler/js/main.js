85a7ce2086ffd0be1a1372b6aecc7476e7b89199
Aaron Reed <aareed@gmail.com>
2015-10-28 11:01:00 -0700

    Core loop in place.

--- StoryAssembler/js/main.js
+++ StoryAssembler/js/main.js
@@ -4,15 +4,57 @@ requirejs.config({
 	paths: {
 		"domReady": "../lib/domReady",
 		"text": "../lib/text",
-		"Display": "Display"
+
+		"underscore": "../lib/underscore-min",
+
+		"util": "util",
+
+		"Display": "Display",
+		"Templates": "Templates"
 	}
 });
 
-requirejs(["Display", "text!../data/sampleChoiceFrame.json", "domReady!"], function(Display, sampleChoiceFrame) {
+requirejs(["Display", "Templates", "text!../data/sampleSceneTemplate.json", "domReady!"], function(Display, Templates, sampleSceneTemplate) {
 	console.log("main.js loaded.");
 
-	var frame = JSON.parse(sampleChoiceFrame);	
+	var handleSelection = function(responseFrameId) {
+		Display.clearAll();
+		processFrame(responseFrameId);
+		doNextFrame();
+	}
 
+	var processFrame = function(frameId) {
+		var frameTemplate = Templates.loadFrame(frameId);
+		var framePlan = frameTemplate.toPlan();
+		framePlan.chunks.forEach(function(chunk) {
+			Display.addStory(chunk);
+		});
+		if (framePlan.choices) {
+			Display.showChoices(framePlan.choices, handleSelection);
+		}
+	}
+
+	var endScene = function() {
+		Display.addStory({"text": "End of scene!"});
+	}
+
+	var doNextFrame = function() {
+		if (scenePosition >= scenePlan.frames.length) {
+			endScene();
+			return;
+		}
+		var frame = scenePlan.frames[scenePosition];
+		console.log("frame", frame);
+		scenePosition++;
+		processFrame(frame.id);	
+	}
+
+	// Load scene and handle first frame.
 	Display.init();
-	Display.showFrame(frame);
+	var sceneTemplate = Templates.loadScene(sampleSceneTemplate);
+	var scenePlan = sceneTemplate.toPlan();
+	console.log("scenePlan", scenePlan);
+	var scenePosition = 0;
+	doNextFrame();
+
 });
---
+++ StoryAssembler/js/util.js
@@ -0,0 +1,58 @@
+
+/* global define, toString */
+
+define([], function() {
+
+	/* Function: isArray
+	Determines if the given object is an array.
+	
+	Parameters:
+	obj - an object
+	
+	Returns:
+	boolean - true if obj is an array.
+	*/
+	var isArray = function(obj) {
+		return toString.call(obj) === "[object Array]";
+	}
+
+	/* Function: clone
+	Creates a clone (non-reference version) of an array or object. Code courtesy A. Levy on Stackoverflow.
+	
+	Parameters:
+	obj - the object to clone
+	
+	Returns:
+	a new object identical to obj
+	*/	
+	var clone = function (obj) {
+	    // Handle the 3 simple types, and null or undefined
+	    if (null === obj || "object" !== typeof obj) return obj;
+
+	    // Handle Array
+	    if (obj instanceof Array) {
+	        var copy = [];
+	        for (var i = 0, len = obj.length; i < len; i++) {
+	            copy[i] = clone(obj[i]);
+	        }
+	        return copy;
+	    }
+
+	    // Handle Object
+	    if (obj instanceof Object) {
+	        var copy = {};
+	        for (var attr in obj) {
+	            if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+	        }
+	        return copy;
+	    }
+
+	    throw new Error("Unable to copy obj! Its type isn't supported.");
+	}
+
+	return {
+		isArray: isArray,
+		clone: clone
+	}
+
+});
