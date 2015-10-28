c2a58bf238f24d11ac869095caf8c5e4c6261e0b
Aaron Reed <aareed@gmail.com>
2015-10-28 11:59:33 -0700

    Factored main loop into StoryAssembler module

--- StoryAssembler/js/main.js
+++ StoryAssembler/js/main.js
@@ -9,52 +9,16 @@ requirejs.config({
 
 		"util": "util",
 
+		"StoryAssembler": "StoryAssembler",
 		"Display": "Display",
 		"Templates": "Templates"
 	}
 });
 
-requirejs(["Display", "Templates", "domReady!"], function(Display, Templates) {
+requirejs(["StoryAssembler", "domReady!"], function(StoryAssembler) {
 	console.log("main.js loaded.");
 
-	var handleSelection = function(responseFrameId) {
-		Display.clearAll();
-		processFrame(responseFrameId);
-		doNextFrame();
-	}
-
-	var processFrame = function(frameId) {
-		var frameTemplate = Templates.loadFrame(frameId);
-		var framePlan = frameTemplate.toPlan();
-		framePlan.chunks.forEach(function(chunk) {
-			Display.addStory(chunk);
-		});
-		if (framePlan.choices) {
-			Display.showChoices(framePlan.choices, handleSelection);
-		}
-	}
-
-	var endScene = function() {
-		Display.addStory({"text": "End of scene!"});
-	}
-
-	var doNextFrame = function() {
-		if (scenePosition >= scenePlan.frames.length) {
-			endScene();
-			return;
-		}
-		var frame = scenePlan.frames[scenePosition];
-		console.log("frame", frame);
-		scenePosition++;
-		processFrame(frame.id);	
-	}
-
 	// Load scene and handle first frame.
-	Display.init();
-	var sceneTemplate = Templates.loadScene("BestFriend");
-	var scenePlan = sceneTemplate.toPlan();
-	console.log("scenePlan", scenePlan);
-	var scenePosition = 0;
-	doNextFrame();
+	StoryAssembler.beginScene("BestFriend");
 
 });
