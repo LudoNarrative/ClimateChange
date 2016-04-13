ef9ad11fcf4456ede9e2c9cb28cd0ce43e1c918d
Aaron Reed <aareed@gmail.com>
2016-04-13 13:25:00 -0700

    Tests on per-module basis

--- StoryAssembler2/js/tests/tests-main.js
+++ StoryAssembler2/js/tests/tests-main.js
@@ -3,20 +3,7 @@
 requirejs.config({
 	paths: {
 		"domReady": "../../lib/domReady",
-		// "text": "../lib/text",
 		"QUnit": "../../lib/qunit-1.23.1",
-		"Tests": "tests",
-
-		// "underscore": "../lib/underscore-min",
-
-		// "util": "util",
-
-		// "StoryAssembler": "StoryAssembler",
-		// "Display": "Display",
-		// "Templates": "Templates",
-		// "Chunks": "Chunks",
-		// "State": "State",
-		// "TextChanger": "TextChanger"
 	},
 	shim: {
        "QUnit": {
@@ -29,16 +16,10 @@ requirejs.config({
     }
 });
 
-requirejs(["QUnit", "Tests", "domReady!"], function(QUnit, Tests) {
+requirejs(["QUnit", "StateTests", "domReady!"], function(QUnit, StateTests) {
 	console.log("SA2 tests-main.js loaded.");
 
-	// Load scene and handle first frame.
-	// State.set("knowCareer", 12);
-	// Display.init(function(){});
-	// Display.addStoryText("Display module working!");
-	// console.log("knowCareer should be 12: ", State.get("knowCareer"));
-	// StoryAssembler.beginScene("BestFriend", characters);
-	Tests.run();
+	StateTests.run();
 
 	QUnit.load();
     QUnit.start();
