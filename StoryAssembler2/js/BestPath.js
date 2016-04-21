2d42e70a16065ecf1e167b2edb0e008799bfd464
Aaron Reed <aareed@gmail.com>
2016-04-20 19:21:24 -0700

    refactoring

--- StoryAssembler2/js/BestPath.js
+++ StoryAssembler2/js/BestPath.js
@@ -1,10 +1,23 @@
 /* Implements the BestPath algorithm for StoryAssembler.
 
-Bestpath takes...
+bestpath takes an array of wants and pointers to the chunk library and state, and return a single path through the chunk library that potentially satisfies the most wants from the list.
+
+The returned path will be an object in the form:
+{
+	route: [], // array of chunkIDs, each step of the path
+	satisfies: [] // array of all possible Want objects that this path makes true.
+}
+
+Note that not everything in "satisfies" will necessarily come to pass: our path might lead through a choice, for instance, but at run time the player selects a different choice. We will really only use the first node in "route" to move the state forward, but we return the whole path in case the rest of the system wants it for some other purpose.
 */
 
 define(["Request", "util"], function(Request, util) {
 
+	// Module-level reference variables
+	var chunkLibrary; 
+	var State;
+
+	// The entry function: does setup, gets the set of all possible paths that satisfy at leat one Want from the list, and returns the best candidate.
 	var bestPath = function(wants, _chunkLibrary, _State) {
 		State = _State;
 		chunkLibrary = _chunkLibrary;
@@ -16,8 +29,11 @@ define(["Request", "util"], function(Request, util) {
 		}
 	}
 
-	var chunkLibrary; // hold local reference
-	var State;
+	// Stub: for now, just return the first option. In the future, this should search for the path that satisfies the most Wants.
+	var chooseFromPotentialPaths = function(paths) {
+		return paths[0];
+	}
+
 
 	// Returns paths from searching every valid chunk in the library.
 	var searchLibraryForPaths = function(wants, okToBeChoice, skipList) {
@@ -26,27 +42,32 @@ define(["Request", "util"], function(Request, util) {
 		for (var i = 0; i < keys.length; i++) {
 			var chunk = chunkLibrary.get(keys[i]);
 
-			// Verify this chunk is valid.
+			// Verify this chunk is valid:
+			// --> it's not blacklisted.
 			if (skipList.indexOf(chunk.id) >= 0) {
 				continue;
 			}
+			// --> the conditions don't disallow it given the current State.
 			if (chunk.conditions) {
 				var shouldContinue = false;
 				for (var j = 0; j < chunk.conditions.length; j++) {
-					var cnd = chunk.conditions[j];
-					if (!State.isTrue(cnd)) {
+					if (!State.isTrue(chunk.conditions[j])) {
 						shouldContinue = true;
 					}
 				}
 				if (shouldContinue) continue;
 			}
+			// --> it's not a response to a choice
 			if (chunk.choiceLabel && (!okToBeChoice)) {
 				continue;
 			}
 
+			// If we made it, consider this chunk and save any found paths
 			var result = findAllSatisfyingPathsFrom(chunk, wants, skipList);
 			paths = paths.concat(result);
 		}
+
+		// Return all discovered paths.
 		return paths;
 	}
 
@@ -54,7 +75,8 @@ define(["Request", "util"], function(Request, util) {
 	var findAllSatisfyingPathsFrom = function(chunk, wants, skipList) {
 		var paths = [];
 		var pathToHere;
-			// Does this chunk directly make one or more wants true?
+		
+		// Does this chunk directly make one or more Wants true?
 		wants.forEach(function(want) {
 			var satisfied = false;
 			if (want.type === "id") {
@@ -65,50 +87,47 @@ define(["Request", "util"], function(Request, util) {
 				}
 			}
 			if (satisfied) {
-				pathToHere = createOrAddWant(pathToHere, chunk.id, want);
+				pathToHere = createPathOrAddWant(pathToHere, chunk.id, want);
 			}
 		});
-		// If so, and there are no outgoing nodes, then the only possible paths from this point are a single one-step path of this chunk, with all the wants it satisfies.
-		if (pathToHere && !chunk.request && !chunk.choices) {
-			return [pathToHere];
-		}
 
-		// Otherwise, see if any outgoing nodes can meet any of our wants. If so, add a path that starts with this node and includes the returned path, and add any met wants to the path.
-
-		// Recurse through all possible paths from this chunk
-		var vPaths, choice, okToBeChoice;
+		// Otherwise, see if any outgoing nodes can meet any of our wants. If so, add paths for each that start with this node, noting any satisfied Wants discovered along the way.
+		var vPaths, req, okToBeChoice;
 		if (chunk.request && chunk.request.type === "id") {
-			choice = Request.byId(chunk.request.val);
+			req = Request.byId(chunk.request.val);
 			okToBeChoice = false;
-			vPaths = findValidPaths(chunk.id, skipList, okToBeChoice, choice, wants, pathToHere);
+			vPaths = findValidPaths(chunk, skipList, req, wants, pathToHere);
 			pathToHere = vPaths.pathToHere;
 			paths = paths.concat(vPaths.paths);
 		}
 		if (chunk.choices) {
 			for (var j = 0; j < chunk.choices.length; j++) {
-				choice = chunk.choices[j];
+				req = chunk.choices[j];
 				okToBeChoice = chunk.choices !== undefined;
-				vPaths = findValidPaths(chunk.id, skipList, okToBeChoice, choice, wants, pathToHere);
+				vPaths = findValidPaths(chunk, skipList, req, wants, pathToHere);
 				pathToHere = vPaths.pathToHere;
 				paths = paths.concat(vPaths.paths);
 			}
 		}
-		// If none of the recursions were successful, but this one was, add it to the list.
+
+		// If nothing beneath this chunk led to a successful path, but this chunk satisfied at least one Want, then the only possible path from this point is a single one-step path to here.
 		if (paths.length === 0 && pathToHere) {
 			return [pathToHere];
 		}
 
+		// Otherwise, we either found something and added it to paths, or we didn't and we'll return an empty array to indicate our search was unsuccessful.
 		return paths;
 	}
 
-	var findValidPaths = function(chunkId, skipList, okToBeChoice, choice, wants, pathToHere) {
-		skipList.push(chunkId);
+	var findValidPaths = function(chunk, skipList, req, wants, pathToHere) {
+		skipList.push(chunk.id);
 		var newWants = util.clone(wants);
-		newWants.push(choice);
+		newWants.push(req);
+		var okToBeChoice = chunk.choices !== undefined;
 		var validPaths = searchLibraryForPaths(newWants, okToBeChoice, skipList);
-		skipList = util.removeFromStringList(skipList, chunkId);
+		skipList = util.removeFromStringList(skipList, chunk.id);
 		if (!pathToHere) {
-			pathToHere = createOrAddWant(pathToHere, chunkId, []);
+			pathToHere = createPathOrAddWant(pathToHere, chunk.id, []);
 		}
 		return {
 			paths: pathsWithValidWant(validPaths, newWants, wants, pathToHere),
@@ -154,7 +173,7 @@ define(["Request", "util"], function(Request, util) {
 		return false;
 	}
 
-	var createOrAddWant = function(path, id, want) {
+	var createPathOrAddWant = function(path, id, want) {
 		if (!path) {
 			path = { // TODO: This needs to be appending to satisfies each time through loop.
 				route: [id],
@@ -164,11 +183,6 @@ define(["Request", "util"], function(Request, util) {
 			path.satisfies.push(want);
 		}
 		return path;
-	}
-
-	var chooseFromPotentialPaths = function(paths) {
-		// Stub: just return the first.
-		return paths[0];
 	}
 
 	return {
