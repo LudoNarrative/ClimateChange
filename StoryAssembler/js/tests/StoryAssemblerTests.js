155718d325ee217e0bb08b871c13d1e7db40e896
Aaron Reed <aareed@gmail.com>
2016-05-13 15:33:09 -0700

    StoryAssembler handles chains of choices correctly now.

--- StoryAssembler/js/tests/StoryAssemblerTests.js
+++ StoryAssembler/js/tests/StoryAssemblerTests.js
@@ -46,6 +46,26 @@ define(["../StoryAssembler", "../ChunkLibrary", "../State", "../Wishlist"], func
 			assert.deepEqual(html(getStoryEl()), "Chunk2 Content", "Basic: Second chunk should be shown correctly");
 			assert.deepEqual(countChildren(getChoiceEl()), 0, "Should be no choices if we've run out of chunks.");
 
+			ChunkLibrary.reset();
+			State.reset();
+			wl = Wishlist.create([{condition: "x eq true"}], State);
+			ChunkLibrary.add([
+				{ id: "Chunk1", content: "Chunk1 Content", choices: [{chunkId: "Chunk2"}] },
+				{ id: "Chunk2", choiceLabel: "Chunk2 Label", content: "Chunk2 Content", choices: [{chunkId: "Chunk3"}] },
+				{ id: "Chunk3", choiceLabel: "Chunk3 Label", content: "Chunk3 Content", effects: ["set x true"] }
+			]);
+			StoryAssembler.beginScene(wl, ChunkLibrary, State);
+			assert.deepEqual(html(getStoryEl()), "Chunk1 Content", "In multi-choice chain, first chunk should be shown correctly");
+			assert.deepEqual(contentForChoice(1), "Chunk2 Label", "In multi-choice chain, first option correct.");
+			clickChoice(1);
+			assert.deepEqual(html(getStoryEl()), "Chunk2 Content", "In multi-choice chain, second chunk shows correctly.");
+			assert.deepEqual(contentForChoice(1), "Chunk3 Label", "In multi-choice chain, second option correct.");
+			assert.deepEqual(countChildren(getChoiceEl()), 1, "In multi-choice chain, should be exactly 1 option.");
+			clickChoice(1);
+			assert.deepEqual(html(getStoryEl()), "Chunk3 Content", "In multi-choice chain, last chunk should show correctly.");
+			assert.deepEqual(countChildren(getChoiceEl()), 0, "In multi-choice chain, no options when finished.");
+
+
 		});
 	}
 
