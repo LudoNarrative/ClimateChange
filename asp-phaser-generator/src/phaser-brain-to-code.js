89ed064942d71668e1e25933454cd547d259e106
Sarah H <smharmon@ucsc.edu>
2016-07-26 13:42:43 -0400

    fix whitespace

--- asp-phaser-generator/src/phaser-brain-to-code.js
+++ asp-phaser-generator/src/phaser-brain-to-code.js
@@ -141,7 +141,8 @@ var writePhaserProgram = function(brain){
               if(addWhitespace){programText+="\n\t"};
               programText += "if(entity.x>game.width){entity.x=game.width;}if (entity.x<0){entity.x=0;} if (entity.y>game.height){entity.y=game.height;}if (entity.y<0){entity.y=0;}"
               if(addWhitespace){programText+="\n\t"};
-              programText += "}}\n";
+              programText += "}}";
+              if(addWhitespace){programText+="\n"};
             }
 
             programText += "};";
