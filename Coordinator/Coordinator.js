59d88bff00a28feb0c1b129110779e906747c054
Jacob <logodaedalus@gmail.com>
2016-09-12 14:43:18 -0700

    Getting levels for Michael's talk set as active, and pathing stuff.

--- Coordinator/Coordinator.js
+++ Coordinator/Coordinator.js
@@ -1,13 +1,14 @@
 console.log(window.location.pathname);
 
-define(["Display", "StoryDisplay", "State", "ChunkLibrary", "Wishlist", "StoryAssembler", "Character","Game", "Hanson", "text!travelData", "text!workerData", "text!lectureData", "text!dinnerData", "text!globalData"], function(Display, StoryDisplay, State, ChunkLibrary, Wishlist, StoryAssembler, Character, Game, Hanson, travelData, workerData, lectureData, dinnerData) {
+define(["Display", "StoryDisplay", "State", "ChunkLibrary", "Wishlist", "StoryAssembler", "Character","Game", "Hanson", "text!travelData", "text!workerData", "text!lectureData", "text!dinnerData", "text!dinner2Data", "text!globalData"], function(Display, StoryDisplay, State, ChunkLibrary, Wishlist, StoryAssembler, Character, Game, Hanson, travelData, workerData, lectureData, dinnerData, dinner2Data) {
 
 	/*
 		Initializing function
 	*/
 	var init = function() {
 
-		var scenes = ["dinner", "lecture", "travel", "worker" ];	//order of scenes
+		//var scenes = ["dinner", "lecture", "travel", "worker" ];	//order of scenes
+		var scenes = ["dinner", "dinner2", "lecture"];	//order of scenes
 		State.set("scenes", scenes);
 		Display.initTitleScreen(this, State, scenes);		//start up UI
 
@@ -24,7 +25,7 @@ define(["Display", "StoryDisplay", "State", "ChunkLibrary", "Wishlist", "StoryAs
 			State.change(command);
 		});
 		var levelData = HanSON.parse(story.dataFile);
-		var globalDataFile = require("text!globalData");
+		var globalDataFile = require("text!globalData");	//TODO: why can't we just include this via require up above with the other data files????
 		var globalData = HanSON.parse(globalDataFile);
 		ChunkLibrary.reset();
 		ChunkLibrary.add(levelData);
@@ -51,7 +52,7 @@ define(["Display", "StoryDisplay", "State", "ChunkLibrary", "Wishlist", "StoryAs
 	var getStorySpec = function(id) {
 
 		var storySpec = [
-		{
+		/*{
 			id: "travel",
 			characters: {
 				"emma" : {name: "Emma", nickname: "Em", gender: "female" }
@@ -100,7 +101,7 @@ define(["Display", "StoryDisplay", "State", "ChunkLibrary", "Wishlist", "StoryAs
 				"set specialty shrimp",
 				"set emotional 0",
 				"set serious chill",
-				"set percent 0",			/*this is what percent is current uncovered*/
+				"set percent 0",			//this is what percent is current uncovered
 				"set requiredPercent 0",
 			],
 			UIvars: [
@@ -112,7 +113,7 @@ define(["Display", "StoryDisplay", "State", "ChunkLibrary", "Wishlist", "StoryAs
 				initiator: "rick",
 				respondent: "emma"
 			}
-		},
+		},*/
 		{
 			id: "lecture",
 			characters: {
@@ -150,6 +151,45 @@ define(["Display", "StoryDisplay", "State", "ChunkLibrary", "Wishlist", "StoryAs
 			/*
 				currently these wishlist items all proceed sequentially
 			*/
+			id: "dinner2",
+			characters: {
+				"emma": {name: "Emma", nickname: "Em", gender: "female"},
+				"zanita": {name: "Zanita", nickname: "Z", gender: "female"},
+				"shelly": {name: "Shelly", nickname: "Shelly", gender: "female"}
+			},
+			wishlist: [
+			//	{ condition: "introMechanics eq true" },
+				{ condition: "establishSpecialtyInfo eq true" },
+				{ condition: "establishDinnerWithFriend eq true" },
+				{ condition: "establishFriendBackstory eq true" },
+				{ condition: "establishEmmaRegrets eq true" },
+				{ condition: "establishEmmaBackstory eq true" },
+				{ condition: "provokeConfidenceChoice eq true" },
+				{ condition: "friendReassuresEmma eq true" },
+		//		{ condition: "droppedKnowledge gte 2", persistent: true },
+			],
+			dataFile: require("text!dinner2Data"),
+			startState: [
+				"set career unpicked",
+				"set droppedKnowledge 0",
+				"set confidence 5",
+				"set patience 5"
+			],
+			UIvars: [
+				"confidence",
+				"patience",
+				"career"
+			],
+			mode: {
+				type: "dialogue",
+				initiator: "zanita",
+				responder: "emma"
+			}
+		},
+		{
+			/*
+				currently these wishlist items all proceed sequentially
+			*/
 			id: "dinner",
 			characters: {
 				"emma": {name: "Emma", nickname: "Em", gender: "female"},
@@ -172,9 +212,11 @@ define(["Display", "StoryDisplay", "State", "ChunkLibrary", "Wishlist", "StoryAs
 				"set career unpicked",
 				"set droppedKnowledge 0",
 				"set confidence 5",
+				"set patience 5"
 			],
 			UIvars: [
 				"confidence",
+				"patience",
 				"career"
 			],
 			mode: {
@@ -193,6 +235,10 @@ define(["Display", "StoryDisplay", "State", "ChunkLibrary", "Wishlist", "StoryAs
 		var sceneScreens = [
 			{
 				id : "dinner",
+				text : "<p>You are Emma Richards, a PhD student who studies the ocean.</p><p>Tomorrow, you'll be defending your thesis. Your friends have decided to throw a dinner party for you.</p><p>Choose what Emma says, but keep an eye on the task you're performing, too!</p>"
+			},
+			{
+				id : "dinner2",
 				text : "<p>You are Emma Richards, a PhD student who studies the ocean.</p><p>Tomorrow, you'll be defending your thesis. Your friends have decided to throw a dinner party for you.</p><p>Choose what Emma says, but keep an eye on the task you're performing, too!</p>"
 			},
 			{
@@ -221,6 +267,10 @@ define(["Display", "StoryDisplay", "State", "ChunkLibrary", "Wishlist", "StoryAs
 				src : "dinner.png"
 			},
 			{
+				id : "dinner2",
+				src : "dinner.png"
+			},
+			{
 				id : "lecture",
 				src : "lecture.png"
 			},
@@ -245,6 +295,26 @@ define(["Display", "StoryDisplay", "State", "ChunkLibrary", "Wishlist", "StoryAs
 		var avatarSpec= [
 			{
 				id : "dinner",
+				avatars: [
+					{
+						id: "happy",
+						src: "happy.png",
+						state: ["confidence gt 4"]
+					},
+					{
+						id: "worried",
+						src: "worried.png",
+						state: ["confidence eq 2"]
+					},
+					{
+						id: "stressed",
+						src: "stressed.png",
+						state: ["confidence eq 0"]
+					},
+				]
+			},
+			{
+				id : "dinner2",
 				avatars: [
 					{
 						id: "happy",
@@ -340,6 +410,11 @@ define(["Display", "StoryDisplay", "State", "ChunkLibrary", "Wishlist", "StoryAs
 		var gameResources = [
 			{
 				id: "dinner",
+				aspFilepaths: ['asp-phaser-generator/test/fixtures/asp-game-1.lp'],
+				gameString : "var variables;  var gridSize;  var gridLinesHorizontal;  var gridLinesVertical;  var grid;  var gridIdx;  var addedEntities;  var low;  var medium;  var mid;  var high;  var goals;  var e1;  var e2;  var r1;  function preload(){  	game.load.image('e1','assets/sprites/circle.png'); 	game.load.image('e2','assets/sprites/circle.png');  }; function create(){  	variables={'confidence':'5','optimism':'2','difficulty':'3'};  	gridSize=30;  	gridLinesHorizontal=Math.floor((game.width-1)/gridSize);  	gridLinesVertical=Math.floor((game.height-1)/gridSize);  	grid=initGrid();  	gridIdx=0;  	addedEntities={};  	low=1;  	medium=6;  	mid=medium;  	high=11;  	goals=[]; 	e1=game.add.physicsGroup();  	addedEntities['e1']=e1;  	initEntityProperties(e1); 	e2=game.add.physicsGroup();  	addedEntities['e2']=e2;  	initEntityProperties(e2);  	r1=high; 	var x=190;var y=160;for (var ii = 0; ii < 1; ii++){  		x+=(Math.random() * 30) - 15;  		y+=(Math.random() * 30) - 15;  		addedEntities['e1'].create(x,y,'e1');  		updateGrid();  		initEntityProperties(addedEntities['e1']);  	}  	var x=50;var y=160;for (var ii = 0; ii < 1; ii++){  		x+=(Math.random() * 30) - 15;  		y+=(Math.random() * 30) - 15;  		addedEntities['e2'].create(x,y,'e2');  		updateGrid();  		initEntityProperties(addedEntities['e2']);  	}  	var x=300;var y=160;for (var ii = 0; ii < 1; ii++){  		x+=(Math.random() * 30) - 15;  		y+=(Math.random() * 30) - 15;  		addedEntities['e2'].create(x,y,'e2');  		updateGrid();  		initEntityProperties(addedEntities['e2']);  	}  	game.time.events.loop(Phaser.Timer.SECOND*10, t1_t1Listener, this); 	addedEntities['e2'].forEach(function(item){item.body.immovable=true;}, this);  } function update(){  	for(var k in addedEntities) {if (addedEntities.hasOwnProperty(k)) {  		var entity = addedEntities[k];  		entity.forEach(function(item) {  		item.body.velocity.x *= 0.9;  		item.body.velocity.y *= 0.9;  		}, this);  	}} 	addedEntities['e1'].forEach(function(item){  		item.inputEnabled=true;  		item.input.enableDrag(true);  	}, this); 	r1=r1-low*this.game.time.elapsed/10000.0; 	game.physics.arcade.overlap(addedEntities['e2'],addedEntities['e1'],o1OverlapHandler,null, this);  	if(r1<=0){  		changeMode('narrative_progression');  } 	addedEntities['e1'].forEach(function(item){item.tint=0xffffff;}, this);  	addedEntities['e2'].forEach(function(item){item.tint=0x0000ff;}, this);  	game.physics.arcade.collide(e1,e1,null,null,this); 	for(var k in addedEntities) {if (addedEntities.hasOwnProperty(k)) {  		var entity = addedEntities[k];  		entity.forEach(function(item) {  		item.body.velocity.clamp(-300,300);  			if(item.x>game.width){item.x=game.width;}if (item.x<0){item.x=0;} if (item.y>game.height){item.y=game.height;}if (item.y<0){item.y=0;}  		}, this);  	}}  }; function render(){};  function t1_t1Listener(){ 	var x=190;var y=160;for (var ii = 0; ii < 1; ii++){  		x+=(Math.random() * 30) - 15;  		y+=(Math.random() * 30) - 15;  		addedEntities['e1'].create(x,y,'e1');  		updateGrid();  		initEntityProperties(addedEntities['e1']);  	}  }  function o1OverlapHandler(e1,e2){  	e2.destroy(); 	r1=r1+low; }; function setVariable(varName,value){  	variables[varName]=value;  	State.set(varName, value);  	StoryAssembler.refreshNarrative();  	Display.setAvatar(State);  }; function getVariable(varName){  	return variables[varName];  }; function getRandomPoint(){  	var x=game.rnd.integerInRange(0,game.world.width-1);  	var y=game.rnd.integerInRange(0,game.world.height-1);  	return new Phaser.Point(x,y);  }; function initGrid(){  	grid=[];  	for(var i=0;i<gridLinesHorizontal;i++){for(var j=0;j<gridLinesVertical;j++){grid.push(new Phaser.Point(i*gridSize,j*gridSize));}}  	shuffle(grid);  	return grid;  }; function updateGrid(sprite){  	gridIdx++;  	if(gridIdx===grid.length){gridIdx=0;shuffle(grid);}  }; function shuffle(a){  	var j,x,i;  	for(i=a.length;i;i--){j=Math.floor(Math.random()*i);x=a[i-1];a[i-1]=a[j];a[j]=x;}  }; function move_towards(e,dir){  	e.body.velocity.x += dir.x;  	e.body.velocity.y += dir.y;  }; function move_away(e,dir){  	e.body.velocity.x -= dir.x;  	e.body.velocity.y -= dir.y;  }; function moves(e,x,y){  	e.body.velocity.x += x;  	e.body.velocity.y += y;  }; function move_forward(e,amount){  	var newV = game.physics.arcade.velocityFromRotation(e.rotation,amount);  	e.body.velocity.x += newV.x;  	e.body.velocity.y += newV.y;  }; function move_left(e,amount){  	var newV = game.physics.arcade.velocityFromRotation(e.rotation-Math.PI*0.5,amount);  	e.body.velocity.x += newV.x;  	e.body.velocity.y += newV.y;  }; function move_right(e,amount){  	var newV = game.physics.arcade.velocityFromRotation(e.rotation+Math.PI*0.5,amount);  	e.body.velocity.x += newV.x;  	e.body.velocity.y += newV.y;  }; function move_backward(e,amount){  	var newV = game.physics.arcade.velocityFromRotation(e.rotation-Math.PI,amount);  	e.body.velocity.x += newV.x;  	e.body.velocity.y += newV.y;  }; function initEntityProperties(group){  	group.forEach(function(item) {  	item.body.collideWorldBounds = true;  	item.anchor.x = 0.5;  	item.anchor.y = 0.5;  	item.rotation = 0;  	if (!item.body.velocity.hasOwnProperty('x')){item.body.velocity.x=0;}  	if (!item.body.velocity.hasOwnProperty('y')){item.body.velocity.y=0;}  	if (!item.body.hasOwnProperty('angularVelocity')){item.body.angularVelocity=0;}  	}, this);  }; function changeMode(newMode){  	if(newMode==='game_win'){mode = 'win'; game.world.removeAll(); displayText('CLEARED');}  	else if(newMode==='game_loss'){mode='loss'; game.stage.backgroundColor = '#400';}  }; function displayText(t){  	var style = { font: 'bold 32px Arial', fill: '#fff', boundsAlignH: 'center', boundsAlignV: 'middle'};  	text = game.add.text(0, 0, t, style);  }; function getAspGoals(){  	if (goals === undefined || goals.length == 0){return ['No ASP goals.'];}  	else{return goals;}  }; goals=['Prevent:[r1] le [0]','Maintain r1'];"
+			},
+			{
+				id: "dinner2",
 				aspFilepaths: ['asp-phaser-generator/test/fixtures/asp-game-1.lp'],
 				gameString : "var variables;  var gridSize;  var gridLinesHorizontal;  var gridLinesVertical;  var grid;  var gridIdx;  var addedEntities;  var low;  var medium;  var mid;  var high;  var goals;  var e1;  var e2;  var r1;  function preload(){  	game.load.image('e1','assets/sprites/circle.png'); 	game.load.image('e2','assets/sprites/circle.png');  }; function create(){  	variables={'confidence':'5','optimism':'2','difficulty':'3'};  	gridSize=30;  	gridLinesHorizontal=Math.floor((game.width-1)/gridSize);  	gridLinesVertical=Math.floor((game.height-1)/gridSize);  	grid=initGrid();  	gridIdx=0;  	addedEntities={};  	low=1;  	medium=6;  	mid=medium;  	high=11;  	goals=[]; 	e1=game.add.physicsGroup();  	addedEntities['e1']=e1;  	initEntityProperties(e1); 	e2=game.add.physicsGroup();  	addedEntities['e2']=e2;  	initEntityProperties(e2);  	r1=high; 	var x=190;var y=160;for (var ii = 0; ii < 1; ii++){  		x+=(Math.random() * 30) - 15;  		y+=(Math.random() * 30) - 15;  		addedEntities['e1'].create(x,y,'e1');  		updateGrid();  		initEntityProperties(addedEntities['e1']);  	}  	var x=50;var y=160;for (var ii = 0; ii < 1; ii++){  		x+=(Math.random() * 30) - 15;  		y+=(Math.random() * 30) - 15;  		addedEntities['e2'].create(x,y,'e2');  		updateGrid();  		initEntityProperties(addedEntities['e2']);  	}  	var x=300;var y=160;for (var ii = 0; ii < 1; ii++){  		x+=(Math.random() * 30) - 15;  		y+=(Math.random() * 30) - 15;  		addedEntities['e2'].create(x,y,'e2');  		updateGrid();  		initEntityProperties(addedEntities['e2']);  	}  	game.time.events.loop(Phaser.Timer.SECOND*10, t1_t1Listener, this); 	addedEntities['e2'].forEach(function(item){item.body.immovable=true;}, this);  } function update(){  	for(var k in addedEntities) {if (addedEntities.hasOwnProperty(k)) {  		var entity = addedEntities[k];  		entity.forEach(function(item) {  		item.body.velocity.x *= 0.9;  		item.body.velocity.y *= 0.9;  		}, this);  	}} 	addedEntities['e1'].forEach(function(item){  		item.inputEnabled=true;  		item.input.enableDrag(true);  	}, this); 	r1=r1-low*this.game.time.elapsed/10000.0; 	game.physics.arcade.overlap(addedEntities['e2'],addedEntities['e1'],o1OverlapHandler,null, this);  	if(r1<=0){  		changeMode('narrative_progression');  } 	addedEntities['e1'].forEach(function(item){item.tint=0xffffff;}, this);  	addedEntities['e2'].forEach(function(item){item.tint=0x0000ff;}, this);  	game.physics.arcade.collide(e1,e1,null,null,this); 	for(var k in addedEntities) {if (addedEntities.hasOwnProperty(k)) {  		var entity = addedEntities[k];  		entity.forEach(function(item) {  		item.body.velocity.clamp(-300,300);  			if(item.x>game.width){item.x=game.width;}if (item.x<0){item.x=0;} if (item.y>game.height){item.y=game.height;}if (item.y<0){item.y=0;}  		}, this);  	}}  }; function render(){};  function t1_t1Listener(){ 	var x=190;var y=160;for (var ii = 0; ii < 1; ii++){  		x+=(Math.random() * 30) - 15;  		y+=(Math.random() * 30) - 15;  		addedEntities['e1'].create(x,y,'e1');  		updateGrid();  		initEntityProperties(addedEntities['e1']);  	}  }  function o1OverlapHandler(e1,e2){  	e2.destroy(); 	r1=r1+low; }; function setVariable(varName,value){  	variables[varName]=value;  	State.set(varName, value);  	StoryAssembler.refreshNarrative();  	Display.setAvatar(State);  }; function getVariable(varName){  	return variables[varName];  }; function getRandomPoint(){  	var x=game.rnd.integerInRange(0,game.world.width-1);  	var y=game.rnd.integerInRange(0,game.world.height-1);  	return new Phaser.Point(x,y);  }; function initGrid(){  	grid=[];  	for(var i=0;i<gridLinesHorizontal;i++){for(var j=0;j<gridLinesVertical;j++){grid.push(new Phaser.Point(i*gridSize,j*gridSize));}}  	shuffle(grid);  	return grid;  }; function updateGrid(sprite){  	gridIdx++;  	if(gridIdx===grid.length){gridIdx=0;shuffle(grid);}  }; function shuffle(a){  	var j,x,i;  	for(i=a.length;i;i--){j=Math.floor(Math.random()*i);x=a[i-1];a[i-1]=a[j];a[j]=x;}  }; function move_towards(e,dir){  	e.body.velocity.x += dir.x;  	e.body.velocity.y += dir.y;  }; function move_away(e,dir){  	e.body.velocity.x -= dir.x;  	e.body.velocity.y -= dir.y;  }; function moves(e,x,y){  	e.body.velocity.x += x;  	e.body.velocity.y += y;  }; function move_forward(e,amount){  	var newV = game.physics.arcade.velocityFromRotation(e.rotation,amount);  	e.body.velocity.x += newV.x;  	e.body.velocity.y += newV.y;  }; function move_left(e,amount){  	var newV = game.physics.arcade.velocityFromRotation(e.rotation-Math.PI*0.5,amount);  	e.body.velocity.x += newV.x;  	e.body.velocity.y += newV.y;  }; function move_right(e,amount){  	var newV = game.physics.arcade.velocityFromRotation(e.rotation+Math.PI*0.5,amount);  	e.body.velocity.x += newV.x;  	e.body.velocity.y += newV.y;  }; function move_backward(e,amount){  	var newV = game.physics.arcade.velocityFromRotation(e.rotation-Math.PI,amount);  	e.body.velocity.x += newV.x;  	e.body.velocity.y += newV.y;  }; function initEntityProperties(group){  	group.forEach(function(item) {  	item.body.collideWorldBounds = true;  	item.anchor.x = 0.5;  	item.anchor.y = 0.5;  	item.rotation = 0;  	if (!item.body.velocity.hasOwnProperty('x')){item.body.velocity.x=0;}  	if (!item.body.velocity.hasOwnProperty('y')){item.body.velocity.y=0;}  	if (!item.body.hasOwnProperty('angularVelocity')){item.body.angularVelocity=0;}  	}, this);  }; function changeMode(newMode){  	if(newMode==='game_win'){mode = 'win'; game.world.removeAll(); displayText('CLEARED');}  	else if(newMode==='game_loss'){mode='loss'; game.stage.backgroundColor = '#400';}  }; function displayText(t){  	var style = { font: 'bold 32px Arial', fill: '#fff', boundsAlignH: 'center', boundsAlignV: 'middle'};  	text = game.add.text(0, 0, t, style);  }; function getAspGoals(){  	if (goals === undefined || goals.length == 0){return ['No ASP goals.'];}  	else{return goals;}  }; goals=['Prevent:[r1] le [0]','Maintain r1'];"
 			},
