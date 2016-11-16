a0402cc0305a2d9fa6ab0edaa494db3e433203da
Adam Summerville <Adam Summerville>
2016-11-16 09:20:44 -0800

    Producing reasonable games, needs structure and parameter reasoning

--- GameGenerator/ASP_cleaned/common.sh
+++ GameGenerator/ASP_cleaned/common.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "generation_constraints.lp generation_rules.lp generation_atoms.lp generation.lp  justification-rules.lp  structure_rules.lp banned_list.lp simplicity_tuning.lp interpretation_rules.lp parameter_tuning.lp"
+echo "generation_constraints.lp generation_rules.lp generation_atoms.lp generation.lp  readings.lp  structure_tuning.lp banned_list.lp simplicity_tuning.lp  parameter_tuning.lp"
