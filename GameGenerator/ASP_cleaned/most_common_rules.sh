88b17153c1e4975baf443a283e12eea6f64f402b
Adam Summerville <Adam Summerville>
2016-11-19 13:13:11 -0800

    Parameter tuning start.

--- GameGenerator/ASP_cleaned/most_common_rules.sh
+++ GameGenerator/ASP_cleaned/most_common_rules.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-clingo "$@" --text > temp; sed -e "s/[0-9]\+/D/g" temp > temp2;  uniq -c temp2 > temp3; sort -nr temp3 > temp4 
+clingo "$@" --text > temp; sed -e "s/[0-9]\+/D/g" temp > temp2;  uniq -c temp2 > temp3; sort -nr temp3 > temp4 ; head temp4
