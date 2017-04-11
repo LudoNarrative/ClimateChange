9cb44c036aec75dc7f181bcdd01a189c2a455b1c
Adam Summerville <adamsumm@gmail.com>
2017-04-10 21:14:27 -0700

    Added in replacements.lp

--- GameGenerator/ASP_cleaned/common.sh
+++ GameGenerator/ASP_cleaned/common.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "generation_constraints.lp generation_rules.lp generation_atoms.lp generation.lp  readings.lp  structure_tuning.lp banned_list.lp simplicity_tuning.lp player_model.lp"
+echo "generation_constraints.lp generation_rules.lp generation_atoms.lp generation.lp  readings.lp  structure_tuning.lp banned_list.lp simplicity_tuning.lp player_model.lp replacements.lp"
