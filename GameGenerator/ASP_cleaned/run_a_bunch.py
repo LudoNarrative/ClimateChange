a3ad8a45dcd14c2d605f02d2a16be07e354b93c1
Adam Summerville <adamsumm@gmail.com>
2017-05-25 13:35:22 -0700

    to the cloud

--- GameGenerator/ASP_cleaned/run_a_bunch.py
+++ GameGenerator/ASP_cleaned/run_a_bunch.py
@@ -6,18 +6,20 @@ args = sys.argv[2:]
 
 
 for ii in range(1,26):
+    print ii
+    print ('python simulate.py {} > {}_{}.lp'.format(' '.join(args),filename,ii))
     os.system('python simulate.py {} > {}_{}.lp'.format(' '.join(args),filename,ii))
     output = []
     with open('{}_{}.lp'.format(filename,ii)) as infile:
         found = False
         for line in infile:
-            if '=' in line:
+            if '========' in line:
                 if found:
                     output.append(line.rstrip())
                 found = True
                 
             elif found:
                 output.append(line.rstrip())
-    
+    print '\n'.join(output)
     with open('{}_{}.lp'.format(filename,ii),'wb') as outfile:
         outfile.write('\n'.join(output))
