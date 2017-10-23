accef5c5ff9b7c3abbff24f11f4a1b7672208300
Adam Summerville <adamsumm@gmail.com>
2017-10-23 15:27:02 -0700

    Fixed some reasoning about clicking

--- GameGenerator/ASP_cleaned/common.sh
+++ GameGenerator/ASP_cleaned/common.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "generation_constraints.lp generation_rules.lp generation_atoms.lp generation.lp  readings.lp  structure_rules.lp banned_list.lp simplicity_tuning.lp player_model.lp replacements.lp project.lp"
+echo "generation_constraints.lp generation_rules.lp generation_atoms.lp generation.lp  readings.lp  structure_rules.lp banned_list.lp simplicity_tuning.lp player_model.lp replacements.lp"
 
